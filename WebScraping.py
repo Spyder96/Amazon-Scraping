92fe22f0e2ff19cd8f8f5b0b5b0d956557f064a4
Spyder96 <Amirul.1216@gmail.com>
2023-04-09 22:15:46 +0530

    splitting into functions

--- WebScraping.py
+++ WebScraping.py
@@ -8,6 +8,7 @@ import datetime
 import psycopg2 as pg
 import time
 from psycopg2 import extras  as pgextras
+import pymongo
 
 def create_database(connection):
     conn=connection
@@ -83,151 +84,185 @@ def soup_table_data(table):
     return data
 
 
-      
+def product_details(link,search,page,Headers):
+    date_time = datetime.datetime.now()
+    current_datetime = date_time.strftime("%Y-%m-%d %H:%M:%S")
+    
+    product_webpage=requests.get(link,headers=Headers)
+    
+    prod_soup = BeautifulSoup(product_webpage.content,"html.parser")
+
+    #product name
+    try:
+        product_name=prod_soup.find("span", attrs={'class': "a-size-large product-title-word-break"}).text.strip()
+    except (AttributeError,ValueError) as e:
+        return 0
+    
+    #price
+    try:    
+        price_span=prod_soup.find("span",attrs={'class': "a-price aok-align-center reinventPricePriceToPayMargin priceToPay"})
+        price_class=price_span.find("span",attrs={'class': "a-price-whole"}).text
+        #price
+        price =  float(''.join(re.findall(r'\d+',price_class)))
+    except (AttributeError,ValueError) as e:
+        price = 0
+
+    #description
+    try :
+        descriptions_list= prod_soup.find("ul",attrs={'class': "a-unordered-list a-vertical a-spacing-mini"}).text.strip().split("    ")
+        #description='\n'.join(descriptions_list)
+        description= json.dumps(descriptions_list)
+    except:
+        description = "No Description available"
+
+    #rating
+    try:
+        rating= prod_soup.find("a",attrs={ 'id':"acrCustomerReviewLink" ,'class':"a-link-normal"})
+
+        #text format eg: 1,765 ratings 
+        rating= rating.text.strip()
+        number_of_ratings=  int(''.join(re.findall(r'\d+', rating)))
+    except (AttributeError,ValueError) as e:
+        number_of_ratings = 0
         
-search = input("enter")
-URL= f"https://www.amazon.in/s?k={search}&crid=2C9GZV1PQRGTM&sprefix=abc%2Caps%2C501&ref=nb_sb_noss_2"
+        
+    #number of stars
+    try:
+        stars= prod_soup.find("a",attrs={'class':"a-popover-trigger a-declarative"}).text
+
+        number_of_stars= float(stars.strip().split()[0])
+    except (AttributeError,ValueError) as e:
+        number_of_stars = 0
+
+
+    #num of answered questions
+    try: 
+        answered_questions= (prod_soup.find("a",attrs={ 'class':"a-link-normal askATFLink"}).text.strip()).split()
+        num_answered_questions = int(''.join(re.findall(r'\d+', answered_questions[0])))
+    except (AttributeError,ValueError) as e:
+        num_answered_questions = 0
+    
+    #Amazon highlights
+    try:
+        features = prod_soup.find_all("a",attrs={ 'class': "a-size-small a-link-normal a-text-normal"})
+        #feature_list = ", ".join([feature.text.strip() for feature in features])
+        feature_list = json.dumps([feature.text.strip() for feature in features])
+    except (AttributeError,ValueError) as e:
+        feature_list = "Not Available"
+        
+    #Scraping techincal data
+    tech_table = prod_soup.find('table', attrs={'class':'a-keyvalue prodDetTable'})
+    technical_details= soup_table_data(tech_table)
+    
+    #Scraping Addional data
+    add_div = prod_soup.find( 'div', attrs={ 'id':"productDetails_db_sections", 'class':"a-section"})
+    add_table = add_div.find( 'table', attrs={ 'id':"productDetails_detailBullets_sections1", \
+                                                  'class':"a-keyvalue prodDetTable"})
+    
+    additional_details= soup_table_data(add_table) 
+    
+    final_product_data = {
+        'ASIN'               : additional_details['ASIN'],
+        'Name'               : product_name,
+        'Price'              : price, 
+        'Stars'              : number_of_stars, 
+        'Ratings'            : number_of_ratings,
+        'Answered_Questions' : num_answered_questions,
+        'Amazon_Services'    : feature_list,
+        'Description'        : description,
+        'Technical_details'  : technical_details,
+        'Additional_details' : additional_details,
+        'Link'               : link,
+        'Page'               : page,
+        'Date'               : current_datetime,
+        'Search'             : search
+        }
+    
+    return final_product_data
+    
+    
+def main_postgres_connection(dbname):
+    
+    if dbname == "postgres":  #postgres connection
+        connection = pg.connect("host=localhost dbname=postgres user=postgres password=admin")
+        connection.autocommit = True
+        create_database(connection)
+        return 0
+    else:
+        #connecting to Amazon database
+        connection = pg.connect("host=localhost dbname=amazon user=postgres password=admin")
+        connection.autocommit = True
+        return connection
+    
+        
+        
+
+      
+def Amazon_search(search):
+    Headers=({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0' , 'Accept-language':'en-US , en;q=0.5'})
+    
+    URL= f"https://www.amazon.in/s?k={search}&crid=2C9GZV1PQRGTM&sprefix=abc%2Caps%2C501&ref=nb_sb_noss_2"
+    pages_available = True
+    page=1
+    
+    while pages_available:
+        
+        webpage=requests.get(URL,headers=Headers)
+        #Creating initial soup file
+        soup = BeautifulSoup(webpage.content,"html.parser")
+        #searching for product links available in the page
+        links=soup.find_all("a",attrs={'class' : 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
+        all_product_data=[]
+        #iterating the reference links for the listed products
+        
+        for link in links:
+            #dates
+            sublink=link.get('href')
+            product_link = "https://www.amazon.in" + sublink
+            
+            data = product_details (product_link,search,page,Headers)
+            
+            if data == 0:
+                continue
+            else:
+                print(data)
+            time.sleep(1)
+            
+            #creating a dataframe of the products
+        #df = pd.DataFrame(all_product_data, columns=['Name', 'Price' , 'Stars', 'Number_of_Ratings', 'Number_of_Answered_Questions', 'Amazon_offerings', 'Brief_Description', 'product_link', 'Page' , 'Date'])
+         
+        #inserting data
+        #table_inserts_df(search, df, connection)
+        #checking for next page
+        try :
+            next_page=soup.find("a",attrs={ 'class': "s-pagination-item s-pagination-next s-pagination-button s-pagination-separator"})
+            next_page_link ="https://www.amazon.in"+ next_page.get('href')
+            page = page+1
+            URL = next_page_link
+            print(f"Page : {page} ")
+        except:
+            pages_available = False
+     
+    
+    
+    
+    
+def main():
+    search = input("Enter what you want to search in Amazon")
+    
+    
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+# list the available databases
+    print(client.list_database_names())
+    #Amazon_search(search)
 #request headers
-Headers=({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0' , 'Accept-language':'en-US , en;q=0.5'})
 
 
-#postgres connection
-conn = pg.connect("host=localhost dbname=postgres user=postgres password=admin")
-conn.autocommit = True
-create_database(conn)
-
-#connecting to Amazon database
-connection = pg.connect("host=localhost dbname=amazon user=postgres password=admin")
-connection.autocommit = True
-
-#Crateing table
-table_name = search
-create_table(connection,table_name)
+main()
+# connection=main_postgres_connection("amazon")
+# #Creating table
+# table_name = search
+# create_table(connection,table_name)
 
 #first page
-pages_available = True
-page=1
-while pages_available:
-    webpage=requests.get(URL,headers=Headers)
-    #Creating initial soup file
-    soup = BeautifulSoup(webpage.content,"html.parser")
-    #searching for product links available in the page
-    links=soup.find_all("a",attrs={'class' : 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
-    all_product_data=[]
-    #getting reference link for next website
-    for link in links:
-        
-        
-        #dates
-        date_time = datetime.datetime.now()
-        current_datetime = date_time.strftime("%Y-%m-%d %H:%M:%S")
-        
-        sublink=link.get('href')
-        product_link = "https://www.amazon.in" + sublink
-        product_webpage=requests.get(product_link,headers=Headers)
-        prod_soup = BeautifulSoup(product_webpage.content,"html.parser")
-
-        #product name
-        try:
-            product_name=prod_soup.find("span", attrs={'class': "a-size-large product-title-word-break"}).text.strip()
-        except (AttributeError,ValueError) as e:
-            continue
-        
-        #price
-        try:    
-            price_span=prod_soup.find("span",attrs={'class': "a-price aok-align-center reinventPricePriceToPayMargin priceToPay"})
-            price_class=price_span.find("span",attrs={'class': "a-price-whole"}).text
-            #price
-            price =  float(''.join(re.findall(r'\d+',price_class)))
-        except (AttributeError,ValueError) as e:
-            price = 0
-
-        #description
-        try :
-            descriptions_list= prod_soup.find("ul",attrs={'class': "a-unordered-list a-vertical a-spacing-mini"}).text.strip().split("    ")
-            #description='\n'.join(descriptions_list)
-            description= json.dumps(descriptions_list)
-        except:
-            description = "No Description available"
-
-
-        #rating
-        try:
-            rating= prod_soup.find("a",attrs={ 'id':"acrCustomerReviewLink" ,'class':"a-link-normal"})
-
-            #text format eg: 1,765 ratings 
-            rating= rating.text.strip()
-            number_of_ratings=  int(''.join(re.findall(r'\d+', rating)))
-        except (AttributeError,ValueError) as e:
-            number_of_ratings = 0
-            
-            
-        #number of stars
-        try:
-            stars= prod_soup.find("a",attrs={'class':"a-popover-trigger a-declarative"}).text
-
-            number_of_stars= float(stars.strip().split()[0])
-        except (AttributeError,ValueError) as e:
-            number_of_stars = 0
-
-
-        #num of answered questions
-        try: 
-            answered_questions= (prod_soup.find("a",attrs={ 'class':"a-link-normal askATFLink"}).text.strip()).split()
-            num_answered_questions = int(''.join(re.findall(r'\d+', answered_questions[0])))
-        except (AttributeError,ValueError) as e:
-            num_answered_questions = 0
-        
-        #Amazon highlights
-        try:
-            features = prod_soup.find_all("a",attrs={ 'class': "a-size-small a-link-normal a-text-normal"})
-            #feature_list = ", ".join([feature.text.strip() for feature in features])
-            feature_list = json.dumps([feature.text.strip() for feature in features])
-        except (AttributeError,ValueError) as e:
-            feature_list = "Not Available"
-            
-        #Scraping techincal data
-        tech_table = prod_soup.find('table', attrs={'class':'a-keyvalue prodDetTable'})
-        technical_details= soup_table_data(tech_table)
-        
-        #Scraping Addional data
-        add_div = prod_soup.find( 'div', attrs={ 'id':"productDetails_db_sections", 'class':"a-section"})
-        add_table = add_div.find( 'table', attrs={ 'id':"productDetails_detailBullets_sections1", \
-                                                      'class':"a-keyvalue prodDetTable"})
-        
-        additional_details= soup_table_data(add_table) 
-        
-        
-        
-        final_product_data = {
-            'ASIN'               : additional_details['ASIN'],
-            'Name'               : product_name,
-            'Price'              : price, 
-            'Stars'              : number_of_stars, 
-            'Ratings'            : number_of_ratings,
-            'Answered_Questions' : num_answered_questions,
-            'Amazon_Services'    : feature_list,
-            'Description'        : description,
-            'Technical_details'  : technical_details,
-            'Additional_details' : additional_details,
-            'Link'               : product_link,
-            'Page'               : page,
-            'Date'               : current_datetime }
-        
-        all_product_data.append(final_product_data)
-        time.sleep(1)
-        
-        #creating a dataframe of the products
-    #df = pd.DataFrame(all_product_data, columns=['Name', 'Price' , 'Stars', 'Number_of_Ratings', 'Number_of_Answered_Questions', 'Amazon_offerings', 'Brief_Description', 'product_link', 'Page' , 'Date'])
-     
-    #inserting data
-    #table_inserts_df(search, df, connection)
-    #checking for next page
-    try :
-        next_page=soup.find("a",attrs={ 'class': "s-pagination-item s-pagination-next s-pagination-button s-pagination-separator"})
-        next_page_link ="https://www.amazon.in"+ next_page.get('href')
-        page = page+1
-        URL = next_page_link
-        print(f"Page : {page} ")
-    except:
-        pages_available = False

2670d10ab491b097afefa0e004f7920c4bf6755c
Spyder96 <Amirul.1216@gmail.com>
2023-04-14 18:25:34 +0530

    Database connections converted to classs

--- Database.py
+++ Database.py
@@ -6,10 +6,11 @@ Created on Thu Apr 13 23:41:05 2023
 """
 import psycopg2 as pg
 from psycopg2 import extras  as pgextras
+import pymongo
 
 class postgres_database:
     
-    def __init__(self,dbname,user,password,host='localhost'):
+    def __init__(self,user,password,dbname='postgres',host='localhost'):
         self.name = dbname
         self.user = user
         self.password = password
@@ -42,16 +43,14 @@ class postgres_database:
             print("Need to connect to postgres database for this method")
             
 
-# db = postgres_database(dbname="amazon", user="postgres", password="admin")
-# db.connect()
-# db.create_database('north')
 
     def create_table(self,table_name,columns):
         # 
         cursor = self.connection.cursor()
         try:
             create_table_query= " CREATE TABLE %s ( %s )   "
-            cursor.execute(create_table_query,(pg.extensions.AsIs(self.table_name),','.join(columns)))
+            cursor.execute(create_table_query,(pg.extensions.AsIs(table_name),pg.extensions.AsIs(','.join(columns))))
+            print(f"{table_name} created")
         except pg.errors.DuplicateTable:
             print("table exists")
         except Exception as e:
@@ -59,21 +58,59 @@ class postgres_database:
             
         cursor.close()
         #connection.close()
-col= ['id bigserial Primary Key', '\nName varchar not null', '\nPrice real  ', '\nStars real ', '\nNumber_of_Ratings int', ' \nNumber_of_Answered_Questions int', ' \nAmazon_offerings text', ' \nBrief_Description text', '\nproduct_link text', '\nPage  int ', '\nDate timestamp']
-# def table_inserts_df(name,df,connection):
-#     tuples = [tuple(x) for x in df.to_numpy()]
-#     cols=','.join(list(df.columns))
-#     cursor = connection.cursor()
-#     insert_query = """insert into %s(%s) values %%s  """ % (name,cols)
-#     try:
-#         pgextras.execute_values(cursor, insert_query, tuples)
-#         connection.commit()
-#     except(Exception, pg.DatabaseError) as error:
-#         print("Error inserting values: %s" % error)
-#         connection.rollback()
-#         cursor.close()
-#         return 1
-#     print('values inserted to db')
-#     cursor.close()
-#     return 0
+#
+    
+    def table_inserts_df(self,name,df):
+            tuples = [tuple(x) for x in df.to_numpy()]
+            cols=','.join(list(df.columns))
+            cursor = self.connection.cursor()
+            insert_query = """insert into %s(%s) values %%s  """ % (name,cols)
+            try:
+                pgextras.execute_values(cursor, insert_query, tuples)
+                self.connection.commit()
+            except(Exception, pg.DatabaseError) as error:
+                print("Error inserting values: %s" % error)
+                self.connection.rollback()
+                cursor.close()
+                return 1
+            print('values inserted to db')
+            cursor.close()
+            return 0
+ 
+
+class mongodb:
+    
+    def __init__(self,db=None,cols=None,host="localhost",port=27017):
+        self.db = db
+        self.cols = cols
+        self.client = None
+        self.host= host
+        self.port = port
+        
+    def Mongodb_connection(self,dbname="Amazondb",collection="Products"):
+        self.client = pymongo.MongoClient(f"mongodb://{self.host}:{self.port}/")
+        self.db = self.client[f"{dbname}"]
+        self.cols = self.db[f"{collection}"]
+        
+        #return mycol
+
+# db = postgres_database(dbname="amazon", user="postgres", password="admin")
+# db.connect()
+# db.create_database('north')
+ 
+# col= ['id bigserial Primary Key', '\nName varchar not null', '\nPrice real  ', '\nStars real ', '\nNumber_of_Ratings int', ' \nNumber_of_Answered_Questions int', ' \nAmazon_offerings text', ' \nBrief_Description text', '\nproduct_link text', '\nPage  int ', '\nDate timestamp']
+# db.create_table(table_name="mytable", columns=col)
+# db = mongodb()
+# db.Mongodb_connection()
+# #print(db.)
+# #print(db.cols.find())
+# # docs = db.collection.find()
+# # for doc in docs:
+# #     print(doc)
+
+
+# db_list = db.cols.find_one()
+
+# for db in db_list:
+#     # print(db)
 
